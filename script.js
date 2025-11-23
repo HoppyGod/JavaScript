3a4357ed3ce049245dba151a5c3c0870dc007511
HoppyGod <gleb-bog@bk.ru>
2025-11-24 01:08:45 +0300

    Add new task

---
+++ script.js
@@ -0,0 +1,46 @@
+let a = 10
+alert(a)
+a = 20
+alert(a)
+
+let firstIphoneYear = 2007
+alert(firstIphoneYear)
+
+let creator = 'Brendan Eich'
+alert(creator)
+
+let num1 = 10
+let num2 = 2
+alert(num1 + num2)
+alert(num1 - num2)
+alert(num1 * num2)
+alert(num1 / num2)
+
+let result = 2 ** 5
+alert(result)
+
+let a1 = 9
+let b1 = 2
+let ostatok = a1 % b1
+alert(ostatok)
+
+let num = 1
+num += 5
+num -= 3
+num *= 7
+num /= 3
+num++
+num--
+alert(num)
+
+let age = promt('Сколько вам лет?')
+alert(age)
+
+let user = {
+	name: 'Ivan',
+	age: 25,
+	isAdmin: true,
+}
+
+let name = prompt('Как вас зовут?')
+alert(`Привет, ${name}!`)
